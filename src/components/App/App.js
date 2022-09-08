1d24d75c0565b4dacb3f6040dd3526d20d5d912b
Zaphaan <jerome.przybylski@gmail.com>
2022-09-08 11:27:48 +0200

    create of the project structure

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+// Import style
+import './app.scss';
+
+// Import components
+import Header from "components/Header/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './header.scss';
+
+const Header = () => {
+    return  <header>
+                TODO: Header
+            </header>
+};
+
+export default Header;
